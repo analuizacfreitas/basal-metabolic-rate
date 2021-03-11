35e1f434e5965666593fa957f486520881efedee
analuizacfreitas <ana.freitas@bandtec.com.br>
2021-03-11 14:50:54 -0300

    comments organization

--- js/main.js
+++ js/main.js
@@ -41,14 +41,11 @@ function calculate() {
     } else {
         div_result.innerHTML += `<p>Erro inesperado. Insira os dados novamente.</p>`
     }
-
-    /* Basic Activity Factor
-    1.2: If you are sedentary (little or no exercise) = BMR x 1.2
-    1.375: If you are lightly active (light exercise/sports 1-3 days/week) = BMR x 1.375
-    1.55: If you are moderately active (moderate exercise/sports 3-5 days/week) = BMR x 1.55
-    1.725: If you are very active (hard exercise/sports 6-7 days a week) = BMR x 1.725
-    1.9: If you are extra active (very hard exercise/sports & physical job or 2x training) = BMR x 1.9
-    
-    Cálculo do Grau de Atividade Física
-    */
 }
+
+/* Harris-Benedict Equation - Basic Activity Factor
+1.2: If you are sedentary (little or no exercise) = BMR x 1.2
+1.375: If you are lightly active (light exercise/sports 1-3 days/week) = BMR x 1.375
+1.55: If you are moderately active (moderate exercise/sports 3-5 days/week) = BMR x 1.55
+1.725: If you are very active (hard exercise/sports 6-7 days a week) = BMR x 1.725
+1.9: If you are extra active (very hard exercise/sports & physical job or 2x training) = BMR x 1.9*/
