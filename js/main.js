4e017462dfbc30d9148e2472d36bc71954f89dfb
analuizacfreitas <ana.freitas@bandtec.com.br>
2021-03-17 08:25:20 -0300

    comment changes and organization

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ function calculate() {
     var weight = Number(input_weight.value);
     var height = Number(input_height.value);
     var age = Number(input_age.value);
-    div_result.innerHTML = `<br><p>Você informou:
-                            Sexo: ${gender} |
-                            Peso: ${weight}Kg |
-                            Altura: ${height}cm |
-                            Idade: ${age} anos</p>`
+        div_result.innerHTML = `<br><p>Você informou:
+                                Sexo: ${gender} |
+                                Peso: ${weight}Kg |
+                                Altura: ${height}cm |
+                                Idade: ${age} anos</p>`
     if (gender === 'F' || gender === 'f') {
         var women_BMR = (10 * weight) + (6.25 * height) - (5.0 * age) - 161;
         var BMR_sedentaryWomen = women_BMR * 1.2;
@@ -43,9 +43,8 @@ function calculate() {
     }
 }
 
-/* Harris-Benedict Equation - Basic Activity Factor
-1.2: If you are sedentary (little or no exercise) = BMR x 1.2
-1.375: If you are lightly active (light exercise/sports 1-3 days/week) = BMR x 1.375
-1.55: If you are moderately active (moderate exercise/sports 3-5 days/week) = BMR x 1.55
-1.725: If you are very active (hard exercise/sports 6-7 days a week) = BMR x 1.725
-1.9: If you are extra active (very hard exercise/sports & physical job or 2x training) = BMR x 1.9*/
+/* 
+References used to calculate BMR and Physical Activity Factor:
+- Mifflin-St Jeor Equation
+- Basic Activity Factor
+*/
